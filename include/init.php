c4d9285eecbecffaa5815ae6b5d63185b55441df
Peter <peterch1ang@yahoo.com>
2015-02-26 22:20:26 +0800

    first commit

---
+++ include/init.php
@@ -0,0 +1,41 @@
+<?php
+
+$nav_btns = array(
+	'home'		=> 'Home',
+	'society'	=> 'Society',
+	'event'		=> 'Events',
+	'library'	=> 'Library',
+	'camp'		=> 'Camps',
+	'contact'	=> 'Contact Us'
+	);
+
+$sub_btns = array(
+	'home' => array(
+		array('title' => 'Latest Updates', 'path' => '')
+		),
+	'society' => array(
+		array('title' => 'About Us', 'path' => ''),
+		array('title' => 'Our History', 'path' => ''),
+		array('title' => 'Advisor and Patron', 'path' => ''),
+		array('title' => 'Committees', 'path' => '')
+		),
+	'event' => array(
+		array('title' => 'Upcoming Events', 'path' => ''),
+		array('title' => 'Featured Posts', 'path' => '')
+		),
+	'library' => array(
+		array('title' => 'Browse', 'path' => ''),
+		array('title' => 'Terms and Conditions', 'path' => '')
+		),
+	'camp' => array(
+		array('title' => 'Camp Nirvana Singapore', 'path' => ''),
+		array('title' => 'Dharma Camp', 'path' => '')
+		),
+	'contact' => array(
+		array('title' => 'Email Us', 'path' => ''),
+		array('title' => 'Member Registration', 'path' => ''),
+		array('title' => 'Sponsorship', 'path' => '')
+		)
+	)
+
+?>
---
+++ include/navbar.php
@@ -0,0 +1,7 @@
+<?php
+foreach ($nav_btns as $key => $page){
+	echo "
+							<a href=\"?page=$key\" class=\"btn btn-default" . ($key == $nav_btn ? ' nav-btn-selected' : '' ) . "\">$nav_btns[$key]</a>";
+}
+
+?>
