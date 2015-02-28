e3303720f8fc145175af649646946aea70b47ecb
Peter <peterch1ang@yahoo.com>
2015-02-28 17:12:22 +0800

    Reworked slideshow

--- page/home.php
+++ page/home.php
@@ -1,31 +1,55 @@
 <div class="row col-condensed">
 	<div class="col-xs-12">
-		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		<div id="_slide-ntubs" class="carousel slide" data-ride="carousel">
 			<!-- Indicators -->
 			<ol class="carousel-indicators">
-				<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-				<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-				<!--<li data-target="#carousel-example-generic" data-slide-to="2"></li>-->
+				<li data-target="#_slide-ntubs" data-slide-to="0" class="active"></li>
+				<li data-target="#_slide-ntubs" data-slide-to="1"></li>
+				<li data-target="#_slide-ntubs" data-slide-to="2"></li>
+				<li data-target="#_slide-ntubs" data-slide-to="3"></li>
 			</ol>
 
 			<!-- Wrapper for slides -->
 			<div class="carousel-inner" role="listbox">
-				<div class="item active" style="background-color: #000; background-image: url('img/slideshow.jpg'); background-repeat: no-repeat; background-position: center center; background-size: contain;">
-					<img src="img/slideshow.jpg" style="visibility: hidden;" alt="">
-					<div class="carousel-caption"></div>
+				<div class="item active">
+					<img src="img/slideshow.jpg" alt="Chania" />
+					<div class="carousel-caption">
+						<h3>Chania</h3>
+						<p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+					</div>
 				</div>
-				<div class="item" style="background-color: #000; background-image: url('img/slideshow1.jpg'); background-repeat: no-repeat; background-position: center center; background-size: contain;">
-					<img src="img/slideshow1.jpg" style="visibility: hidden;" alt="">
-					<div class="carousel-caption"></div>
+
+				<div class="item">
+					<img src="img/slideshow.jpg" alt="Chania" />
+					<div class="carousel-caption">
+						<h3>Chania</h3>
+						<p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+					</div>
+				</div>
+
+				<div class="item">
+					<img src="img/slideshow.jpg" alt="Flower" />
+					<div class="carousel-caption">
+						<h3>Flowers</h3>
+						<p>Beatiful flowers in Kolymbari, Crete.</p>
+					</div>
+				</div>
+
+				<div class="item">
+					<img src="img/slideshow.jpg" alt="Flower">
+					<div class="carousel-caption">
+						<h3>Flowers</h3>
+						<p>Beatiful flowers in Kolymbari, Crete.</p>
+					</div>
 				</div>
 			</div>
 
-			<!-- Controls -->
-			<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+			<!-- Left and right controls -->
+			<a class="left carousel-control" href="#_slide-ntubs" role="button" data-slide="prev">
 				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 				<span class="sr-only">Previous</span>
 			</a>
-			<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+			<a class="right carousel-control" href="#_slide-ntubs" role="button" data-slide="next">
 				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 				<span class="sr-only">Next</span>
 			</a>
