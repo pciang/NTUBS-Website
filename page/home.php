d49b88ad8c48251814d4d6a660b122100cdd6931
Peter <peterch1ang@yahoo.com>
2015-03-02 15:22:16 +0800

    Finishing upcoming events part

--- page/home.php
+++ page/home.php
@@ -1,4 +1,4 @@
-<div class="row col-condensed">
+<div class="row">
 	<div class="col-xs-12">
 		<div id="_slide-ntubs" class="carousel slide" data-ride="carousel">
 			<!-- Indicators -->
@@ -57,8 +57,8 @@
 	</div>
 </div>
 <div class="row">
-	<div class="page-header">
-		<h3 class="text-center">Upcoming Events</h3>
+	<div class="page-header border-bottom">
+		<h2 class="text-center">Upcoming Events</h3>
 	</div>
 </div>
 <div class="row col-equal">
@@ -100,5 +100,20 @@
 	</div>
 </div>
 <div class="row">
-	
+	<div class="col-sm-4">
+		<h3 class="border-bottom">Recent Post</h3>
+	</div>
+	<div class="col-sm-8">
+		<h3 class="border-bottom">About NTU Buddhist Society</h3>
+	</div>
+</div>
+<div class="row">
+	<div class="col-sm-4">
+		List of recent posts.
+	</div>
+	<div class="col-sm-8">
+		<p>Nanyang Technological University Buddhist Society is a non-sectarian Buddhist society which provides various Dhamma learning experience suitable for NTU community.It envisions propagating the Dhamma and at the same time encouraging spiritual development.</p>
+		<p>Walk the journey together with us to learn the noble knowledge, to practice the wholesome actions, to realize the potential and to share the benefits with all beings.</p>
+		<p>Explore it by yourself. Enhance your campus life with Buddhism today.</p>
+	</div>
 </div>
